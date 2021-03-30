9a5d0758b41641f3dea7c0ee7c4e364bd4fed555
sachiya <sachiya.rz@gmail.com>
2021-03-30 22:45:20 +0900

    lesson05

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+
+
+
+
+
 app_name = 'mamazon'
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
 ]
